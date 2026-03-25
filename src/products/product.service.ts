d260c064b7922600a99d972f1df258e002ebcb3c
event <event@z2t3c3.42bangkok.com>
2026-03-25 10:35:44 +0700

    p01

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -8,7 +8,7 @@ export class ProductsService {
   findAll(): Products[] {
     const filepath = path.join(process.cwd(), 'data', 'products.json');
     const fileData = fs.readFileSync(filepath, 'utf-8');
-    const products: Products[] = JSON.parse(fileData);
+    const products: Products[] = JSON.parse(fileData) as Products[];
 
     return products;
   }
